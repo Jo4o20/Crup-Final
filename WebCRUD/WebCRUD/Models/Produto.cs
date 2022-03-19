dada8e772fd127160e30a31d52ed9a424f8350db
João Gabriel <jgjcph@gmail.com>
2022-03-18 22:07:16 -0400

    final
    ┗( T﹏T )┛

---
+++ WebCRUD/WebCRUD/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCRUD.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+        [Column("produto_nome")]
+        [Display(Name = "nome")]
+        public string? produto_nome { get; set; }
+        [Column("valor")]
+        [Display(Name = "Valor")]
+        public decimal valor { get; set; }
+        [Column("data_vencimento")]
+        [Display(Name = "Validade")]
+        public DateTime data_vencimento { get; set; }
+    }
+}
+
---
+++ WebCRUD/WebCRUD/README.md
@@ -0,0 +1,11 @@
+﻿#Add-Migration Initial-criacao -Context Contexto
+ou
+#dotnet ef migrations add InitialCreate -Context Contexto
+em seguida, pra criar a database
+#Update-Database -Context Contexto
+ou
+#dotnet ef database update -Context Contexto
+
+
+não consegui conectar com o servidor do banco de dados no compuatador
+e tambem não consegui implemenatar nesse com angular
